dec775ba21ea3b887eda521fec47b55a3b1c5e63
vibhav chirravuri <44679811+csvultrainstinct@users.noreply.github.com>
2025-08-14 22:49:54 -0700

    Create Increasing Array.cpp

---
+++ Introductory Problems/Increasing Array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+
+ll IncreasingArray(ll n, vector<ll> &a){
+    ll ans=0;
+    
+    for(auto i=0;i<n-1;i++){
+        
+        if(a[i]>a[i+1]){
+            
+            ans+=abs(a[i]-a[i+1]);
+        }
+    }
+    
+    return ans;
+}
+
+
+int main()
+{
+    ll n;
+    cin>>n;
+    
+    vector<ll>a;
+    for(auto i=0;i<n;i++){
+        ll ins;
+        cin>>ins;
+        a.push_back(ins);
+    }
+    
+    cout<<IncreasingArray(n,a)<<endl;
+
+    return 0;
+}
